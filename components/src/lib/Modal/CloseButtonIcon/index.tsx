a33404ec05b1fc232e593d0ae32f19729ce6197a
Kim Da Eun <midekuna@gmail.com>
2024-05-10 00:07:18 +0900

    chore: step2에서 제거된 파일 재삭제

--- components/src/lib/Modal/CloseButtonIcon/index.tsx
+++
@@ -1,6 +0,0 @@
-import CloseButtonImg from '../../assets/images/closeButton.svg';
-import styles from './style.module.css';
-
-export default function CloseButtonIcon() {
-  return <img className={styles.closeBtn} src={CloseButtonImg} />;
-}
